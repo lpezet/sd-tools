32b2b0de9e4f86b7d2a9543bc7872034de993e8a
Luke Pezet <lpezet@gmail.com>
2023-07-09 10:12:58 -0600

    Added support to read model versions from file. Added DRYRUN mode and DEBUG variable for more verbose run.

--- civitai-downloader.sh
+++ civitai-downloader.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-modelVersion=$1
+modelVersionOrFilename=$1
 outputFolder=$2
+DRYRUN=${DRYRUN:-0}
+DEBUG=${DEBUG:-0}
 
 prerequisites() {
   local TEST=`(which jq > /dev/null) && echo "Present" || echo "Missing"`
@@ -13,15 +15,16 @@ prerequisites
 
 help() {
   echo -e "Usage:\n"
-  echo -e "$0 model_version [output_folder]\n"
+  echo -e "$0 [model_version | filename] [output_folder]\n"
   echo -e "with\n"
   echo -e " model_version\t# The version of the model you want to download.\n"
+  echo -e " filename\t# Path to a file containing model version ids on each line. Lines starting with # will be ignored.\n"
   echo -e " output_folder\t# If provided, download model into that folder.\n"
   echo -e "\n\nIf .config file provided, folder configuration will be loaded from it."
   exit 1
 }
 
-if [ -z "$modelVersion" ]; then
+if [ -z "$modelVersionOrFilename" ]; then
   help
 fi
 
@@ -49,7 +52,8 @@ fi
 
 describe_model() {
   local ver=$1
-  curl -o /tmp/civitai_res.json "https://civitai.com/api/v1/model-versions/${ver}"
+  [[ $DEBUG -ne 0 ]] && echo "# Getting model info for model version ${ver}..."
+  curl -s -o /tmp/civitai_res.json "https://civitai.com/api/v1/model-versions/${ver}"
   if [ $? -eq 1 ]; then
     echo "Error while getting model informaton from CivitAI API. Exiting."
     exit 1
@@ -62,37 +66,65 @@ describe_model() {
   DOWNLOAD_URL="https://civitai.com/api/download/models/${ver}"
 }
 
-describe_model $modelVersion
-if [ -z "$outputFolder" ]; then
-  # Checkpoint, TextualInversion, Hypernetwork, AestheticGradient, LORA, Controlnet, Poses)
-  case ${MODEL_TYPE} in
-    Checkpoint)
-      outputFolder=${OUTPUT_DIR_MODEL}
-      ;;
-    TextualInversion)
-      outputFolder=${OUTPUT_DIR_EMBEDDINGS}
-      ;;
-    Hypernetwork | AestheticGradient | Controlnet | Poses)
-      echo "${MODEL_TYPE} models not yet supported."
-      exit 1
-      ;;
-    LORA)
-      outputFolder=${OUTPUT_DIR_LORA}
-      ;;
-    *)
-      echo "${MODEL_TYPE} models not yet supported (2)."
-      exit1
-  esac
-fi
 
-if [ ! -z "$outputFolder" -a ! -d "$outputFolder" ]; then
-  mkdir -p "$outputFolder"
-fi
+download() {
+  local modelVersion=$1
+  local outputFolder=$2
 
-if [ -z "$outputFolder" ]; then
-  echo "WARNING: Missing output folder. Will download to current directory."
-  outputFolder="./"
-fi
+  describe_model $modelVersion
+  if [ -z "$outputFolder" ]; then
+    # Checkpoint, TextualInversion, Hypernetwork, AestheticGradient, LORA, Controlnet, Poses)
+    case ${MODEL_TYPE} in
+      Checkpoint)
+        outputFolder=${OUTPUT_DIR_MODEL}
+        ;;
+      TextualInversion)
+        outputFolder=${OUTPUT_DIR_EMBEDDINGS}
+        ;;
+      Hypernetwork | AestheticGradient | Controlnet | Poses)
+        echo "${MODEL_TYPE} models not yet supported."
+        exit 1
+        ;;
+      LORA)
+        outputFolder=${OUTPUT_DIR_LORA}
+        ;;
+      *)
+        echo "${MODEL_TYPE} models not yet supported (2)."
+        exit1
+    esac
+  fi
 
-echo "$modelVersion" >> /tmp/sd-tools.civitai-downloader.replay
-wget -P "${outputFolder}" "${DOWNLOAD_URL}" --content-disposition
+  if [ ! -z "$outputFolder" -a ! -d "$outputFolder" ]; then
+    mkdir -p "$outputFolder"
+  fi
+
+  if [ -z "$outputFolder" ]; then
+    echo "WARNING: Missing output folder. Will download to current directory."
+    outputFolder="./"
+  fi
+
+  if [ $DRYRUN -eq 0 ]; then
+    echo "# Downloading model version [${modelVersion}] (${MODEL_TYPE})..."
+    echo "$modelVersion" >> /tmp/sd-tools.civitai-downloader.replay
+    echo wget -P "${outputFolder}" "${DOWNLOAD_URL}" --content-disposition
+  else
+    echo "# In DRY RUN mode. Not downloading model version [${modelVersion}] (${MODEL_TYPE})."
+  fi
+}
+
+
+if [ -f "$modelVersionOrFilename" ]; then
+  echo "Reading model versions from file [$modelVersionOrFilename]..."
+  while IFS= read -r line
+  do
+      # display $line or do somthing with $line
+      [[ $DEBUG -ne 0 ]] && printf 'line=[%s]\n' "$line"
+      if [[ $line =~ ^#.* || -z "$line" ]]; then
+        [[ $DEBUG -ne 0 ]] && echo "Skipping line."
+        continue
+      fi
+      download "$line" "$outputFolder"
+  done <"$modelVersionOrFilename"
+else
+  download "$modelVersionOrFilename" "$outputFolder"
+fi
